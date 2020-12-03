d148bea69df2d1e311ac8a2e4de2a9cd20360d80
AkhilVenkateshPillai <akhiltechwiz@gmail.com>
2020-12-03 22:25:22 +0530

    Feat
    Made css changes, implemented functioning api

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
-var clickNotif=document.querySelector("#btn-translate");
+var clickNotif = document.querySelector("#btn-translate");
+var trnsltContent = document.querySelector("#txt-input");
+var output = document.querySelector("#output");
 
-clickNotif.addEventListener("click",()=>{console.log("it's working")});
+var APIurl = 'https://api.funtranslations.com/translate/gungan.json?text=' + trnsltContent.value+''
+
+console.log(APIurl);
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+const notifHandler = () => {
+
+
+    fetch(APIurl).then(response => response.json()).then(json => { 
+        var content = json.contents.translated;
+        output.innerHTML = content; 
+        
+    }).catch(errorHandler)
+
+}
+
+clickNotif.addEventListener("click", notifHandler);
+
